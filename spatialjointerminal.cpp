df030e18cf7a483689547b70fd15179324b64dc3
Aaditya Mukherjee <79925419+AdiM3500@users.noreply.github.com>
2022-07-09 17:38:07 -0700

    Add files via upload

--- spatialjointerminal.cpp
+++ spatialjointerminal.cpp
@@ -54,7 +54,7 @@ public:
 
 	~RectangleSet() {
 
-		std::cout << "object destroyed"<<std::endl;
+		std::cout << "object destroyed" << std::endl;
 	}
 };
 
@@ -72,10 +72,10 @@ spatial_join_list_t RectangleSet::naive_spatial_join(rectangle_list_t R, rectang
 		for (auto j = 0; j < S.size(); j++) {
 
 			//condition to check if a particular MBR object in R overlaps with ANY objects in S
-			if ((R[i].first.first <= S[j].second.first)
-				&& (S[j].first.first <= R[i].second.first)
-				&& (R[i].first.second <= S[j].second.second)
-				&& (S[j].first.second <= R[i].second.second)
+			if ((R[i].first.first < S[j].second.first)
+				&& (S[j].first.first < R[i].second.first)
+				&& (R[i].first.second < S[j].second.second)
+				&& (S[j].first.second < R[i].second.second)
 				) {
 
 				spatial_list.push_back({ R[i], S[j] });
@@ -84,7 +84,7 @@ spatial_join_list_t RectangleSet::naive_spatial_join(rectangle_list_t R, rectang
 
 			}
 
-			
+
 
 		}
 	}
@@ -98,36 +98,80 @@ spatial_join_list_t RectangleSet::spatial_join(rectangle_list_t R, rectangle_lis
 
 	//R x S
 	spatial_join_list_t spatial_list;
-	spatial_list.reserve(10000000);   
+	spatial_list.reserve(10000000);
 
 	//outer loop iterating through R
-	for (auto i = 0; i < R.size() - 99; i+=100) {
+	for (auto i = 0; i < R.size() - 2499; i += 2500) {
 
 		//inner loop iterating through S (S.sizr() - 3 to not go out of bounds) via loop unrolling
-		for (auto j = 0; j < S.size(); j++) {
+		for (auto j = 0; j < S.size() ; j++) {
 
-			for (int k = 0 ; k < 100; k++) {
+			for (auto k = 0; k < 2500; k+=4) {
+
+				//8 Rectangles of set R disintegrated into its smallest elements i.e. coordinates, which are 32 bit integers. Packed into an _m128i datatype. Each _m128i datatype can store 4 32-bit integers
+				__m128i setR_min_x = _mm_set_epi32(R[i + k].first.first, R[i + k + 1].first.first, R[i + k + 2].first.first, R[i + k + 3].first.first);
+				__m128i setR_min_y = _mm_set_epi32(R[i + k].first.second, R[i + k + 1].first.second, R[i + k + 2].first.second, R[i + k + 3].first.second);
+				__m128i	setR_max_x = _mm_set_epi32(R[i + k].second.first, R[i + k + 1].second.first, R[i + k + 2].second.first, R[i + k + 3].second.first);
+				__m128i	setR_max_y = _mm_set_epi32(R[i + k].second.second, R[i + k + 1].second.second, R[i + k + 2].second.second, R[i + k + 3].second.second);
+
+				//8 Rectangles of set S disintegrated into its smallest elements i.e. coordinate values, which are 32 bit integers. Packed into 4 _m128i datatypes. Each _m128i datatype can store 4 32-bit integers
+
+				__m128i setS_min_x = _mm_set_epi32(S[j].first.first, S[j].first.first, S[j].first.first, S[j].first.first);
+				__m128i setS_min_y = _mm_set_epi32(S[j].first.second, S[j].first.second, S[j].first.second, S[j].first.second);
+				__m128i	setS_max_x = _mm_set_epi32(S[j].second.first, S[j].second.first, S[j].second.first, S[j].second.first);
+				__m128i	setS_max_y = _mm_set_epi32(S[j].second.second, S[j].second.second, S[j].second.second, S[j].second.second);
 
 
+				auto first_comparison = _mm_cmplt_epi32(setR_min_x, setS_max_x);
+				auto second_comparison = _mm_cmpgt_epi32(setR_max_x, setS_min_x);
+				auto third_comparison = _mm_cmplt_epi32(setR_min_y, setS_max_y);
+				auto fourth_comparison = _mm_cmpgt_epi32(setR_max_y, setS_min_y);
 
-				if ((R[i + k].first.first <= S[j].second.first)
-					&& (S[j].first.first <= R[i + k].second.first)
-					&& (R[i + k].first.second <= S[j].second.second)
-					&& (S[j].first.second <= R[i + k].second.second)
+
+				int overlap_satisfied_index[4];
+
+				overlap_satisfied_index[0] = _mm_extract_epi32(first_comparison, 3) * _mm_extract_epi32(second_comparison, 3) * _mm_extract_epi32(third_comparison, 3) * _mm_extract_epi32(fourth_comparison, 3);
+				overlap_satisfied_index[1] = _mm_extract_epi32(first_comparison, 2) * _mm_extract_epi32(second_comparison, 2) * _mm_extract_epi32(third_comparison, 2) * _mm_extract_epi32(fourth_comparison, 2);
+				overlap_satisfied_index[2] = _mm_extract_epi32(first_comparison, 1) * _mm_extract_epi32(second_comparison, 1) * _mm_extract_epi32(third_comparison, 1) * _mm_extract_epi32(fourth_comparison, 1);
+				overlap_satisfied_index[3] = _mm_extract_epi32(first_comparison, 0) * _mm_extract_epi32(second_comparison, 0) * _mm_extract_epi32(third_comparison, 0) * _mm_extract_epi32(fourth_comparison, 0);
+
+
+				for (auto ii = 0; ii < 4; ii++) {
+
+					if (overlap_satisfied_index[ii] == 1) {
+
+						spatial_list.push_back({ R[i + k + ii], S[j] });
+						joins_count += 1;
+					}
+				}
+
+			}
+				
+		//		__mmask8 second_comparison = _mm_cmpge_epi32_mask(setR_max_x, setS_min_x);
+		//		__mmask8 third_comparison = _mm_cmple_epi32_mask(setR_min_y, setS_max_y);
+		//		__mmask8 fourth_comparison = _mm_cmpge_epi32_mask(setR_max_y, setS_min_y);
+
+
+			/*	if ((R[i + k].first.first < S[j].second.first)
+					&& (S[j].first.first < R[i + k].second.first)
+					&& (R[i + k].first.second < S[j].second.second)
+					&& (S[j].first.second < R[i + k].second.second)
 					) {
 
 					spatial_list.push_back({ R[i + k], S[j] });
 					joins_count += 1;
 
 				}
+				*/
 
-			}
+
 			
+
 
 
 			//condition to check if a particular MBR object in R overlaps with ANY objects in S
-			
-			
+
+
 
 		}
 	}
@@ -184,7 +228,7 @@ int main() {
 	auto setR = random_rectangle_generator();
 	auto setS = random_rectangle_generator();
 
-	/* FOR DISPLAYING PURPOSES TO SEE THE RECTANGLES IN BOTH SETS 
+	/* FOR DISPLAYING PURPOSES TO SEE THE RECTANGLES IN BOTH SETS
 	std::cout << "set R\n";
 	for (int n = 0; n < 10; ++n) {
 
@@ -197,7 +241,7 @@ int main() {
 
 		std::cout << setR[n].second.first << ' ';
 
-		
+
 
 		std::cout << setR[n].second.second << std::endl;
 
@@ -226,14 +270,14 @@ int main() {
 	*/
 
 
-	
+
 
 	RectangleSet R(setR);
 	RectangleSet S(setS);
 
 
 
-// **********OPTIMIZED JOIN FUNCTION BENCHMARKING**************
+	// **********OPTIMIZED JOIN FUNCTION BENCHMARKING**************
 	std::cout << std::endl;
 
 	spatial_join_list_t JOIN;
@@ -252,10 +296,10 @@ int main() {
 	std::cout << "Time in microseconds (optimized approach): " << elapsed_time.count() << "us" << std::endl;
 
 	std::cout << "Number of joins (optimized approach): " << joins_count << std::endl;
-	
 
 
-//**********NAIVE JOIN FUNCTION BENCHMARKING**********************
+
+	//**********NAIVE JOIN FUNCTION BENCHMARKING**********************
 	std::cout << std::endl;
 
 	spatial_join_list_t OLD_JOIN;
@@ -275,9 +319,9 @@ int main() {
 	std::cout << "Time in microseconds (naive approach): " << naive_elapsed_time.count() << "us" << std::endl;
 
 
-	std::cout << "Number of joins (naive approach): " << naive_joins_count << std::endl <<std::endl;
+	std::cout << "Number of joins (naive approach): " << naive_joins_count << std::endl << std::endl;
 
-	
+
 
 	/* FOR DISPLAYING FIRST 5 JOINS BETWEEN R AND S
 	for (auto i = 0; i < 5 ; i++) {
@@ -291,9 +335,8 @@ int main() {
 	}
 
 	*/
-	
+
 
 }
-
 
 
